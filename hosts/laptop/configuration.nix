dbcf9fdd68e802d963ee6e10a982b98c576cd88d
Sw3d15h-F1s4 <samjt36@gmail.com>
2024-06-19 19:30:03 -0400

    add qemu to my laptop
    curse you national instruments!

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,3 +1,4 @@
+# Settings specific to my latpop
 { ... }:
 
 {
@@ -10,17 +11,21 @@
   myNixOS = {
     bundles.plasma-desktop.enable = true;
     bundles.users.enable = true;
-    bundles.engineering.enable = true;
 
+    bundles.engineering.enable = true;
+    # bundles.productivity.enable = true;
+
+    amdgpu.enable = false;
+    nvidiagpu.enable = true;
+    bluetooth.enable = true;
     grub.enable = true;
     steam.enable = true;
     nix.enable = true;
     locale.enable = true;
-    nvidiagpu.enable = true;
-    amdgpu.enable = false;
-    bluetooth.enable = true;
+    # aagl.enable = true;
 
     matlab.enable = true;
+    qemu.enable = true;
 
     home-users = {
       sam = {
