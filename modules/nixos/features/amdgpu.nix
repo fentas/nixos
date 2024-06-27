ac053e3b35c91816a43d9b6d84d057924f2264a6
Sw3d15h-F1s4 <samjt36@gmail.com>
2024-06-27 00:11:27 -0400

    move to new amdgpu module

--- modules/nixos/features/amdgpu.nix
+++ modules/nixos/features/amdgpu.nix
@@ -1,17 +1,21 @@
 { pkgs, ... }:
 {
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
+  # hardware.opengl = {
+  #   enable = true;
+  #   driSupport32Bit = true;
+  #
+  #   extraPackages = with pkgs; [
+  #     rocmPackages.clr.icd
+  #   ];
+  # };
+  #
+  # boot.initrd.kernelModules = [ "amdgpu" ];
 
-    extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-    ];
+  # new amdgpu module does all of the above, essentially.
+  hardware.amdgpu = {
+    initrd.enable = true;
+    opencl.enable = true;
   };
-
   services.xserver.videoDrivers = [ "amdgpu" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
-
 
 }
