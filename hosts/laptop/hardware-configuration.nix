cef7be080cc464890193d0bbdc1aa1fc535efdc6
Sw3d15h-F1s4 <samjt36@gmail.com>
2024-07-28 15:31:05 -0400

    add swap partition
    additionally, update disk partition to use labels instead of UUID.
    
    It's a bit too late to invest in Disko.

--- hosts/laptop/hardware-configuration.nix
+++ hosts/laptop/hardware-configuration.nix
@@ -10,16 +10,18 @@
   };
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c4d9c5c0-5c03-4917-9844-5296b74d274f";
+    { device = "/dev/disk/by-label/nvme";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/1FE4-02AB";
+    { device = "/dev/disk/by-label/BOOT";
       fsType = "vfat";
     };
 
-  swapDevices = [ ];
+  swapDevices =
+    [ { device = "/dev/disk/by-label/swap"; }
+    ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
