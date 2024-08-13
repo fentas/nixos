fbff0ee047572c9e9f54fd4fdea1de3e3c41caac
Sw3d15h-F1s4 <samjt36@gmail.com>
2024-08-13 15:15:25 -0400

    add printing support

--- modules/nixos/features/networking.nix
+++ modules/nixos/features/networking.nix
@@ -1,7 +1,17 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 
 {
   networking.networkmanager.enable = true;
   networking.useDHCP = lib.mkDefault true;
-  services.printing.enable = true;
+
+  services.printing = {
+    enable = true;
+    drivers = [ pkgs.hplipWithPlugin ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 }
