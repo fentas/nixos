80a9346b9d7e4e6d980795b6df4e5439848cd5b2
Sw3d15h-F1s4 <samjt36@gmail.com>
2024-08-13 15:17:18 -0400

    disable the funny pipewire hacks
    ill have to figure something out later i guess

--- modules/nixos/features/pipewire/default.nix
+++ modules/nixos/features/pipewire/default.nix
@@ -11,52 +11,52 @@
 
 
     extraConfig = {
-      pipewire = {
-        "99-custom-sinks"= {
-          "context.objects" = [
-            {
-              factory = "adapter";
-              args = {
-                "factory.name" = "support.null-audio-sink";
-                "node.name" = "Firefox-Audio";
-                "node.description" = "All audio from Firefox";
-                "media.class" = "Audio/Duplex";
-                "object.linger" = true;
-                "audio.position" = "[ FL FR ]";
-                "monitor.passthrough" = true;
-              };
-            }
-          ];
-        };
-      };
+      # pipewire = {
+      #   "99-custom-sinks"= {
+      #     "context.objects" = [
+      #       {
+      #         factory = "adapter";
+      #         args = {
+      #           "factory.name" = "support.null-audio-sink";
+      #           "node.name" = "Firefox-Audio";
+      #           "node.description" = "All audio from Firefox";
+      #           "media.class" = "Audio/Duplex";
+      #           "object.linger" = true;
+      #           "audio.position" = "[ FL FR ]";
+      #           "monitor.passthrough" = true;
+      #         };
+      #       }
+      #     ];
+      #   };
+      # };
     };
 
     wireplumber = {
-      extraConfig = {
-        "log-level-debug" = {
-          "context.properties" = {
-            "log.level" = "D";
-          };
-        };
-
-        "99-auto-link" = {
-          "wireplumber.components" = [
-            {
-              name = "custom/auto-link.lua";
-              type = "script/lua";
-              provides = "custom.auto-link";
-            }
-          ];
-          "wireplumber.profiles" = {
-            main = {
-              "custom.auto-link" = "required";
-            };
-          };
-        };
-      };
-      extraScripts = {
-        "custom/auto-link.lua" = builtins.readFile ./auto-link.lua;
-      };
+      # extraConfig = {
+      #    "log-level-debug" = {
+      #      "context.properties" = {
+      #        "log.level" = "D";
+      #      };
+      #   };
+      #
+      #   "99-auto-link" = {
+      #     "wireplumber.components" = [
+      #       {
+      #         name = "custom/auto-link.lua";
+      #         type = "script/lua";
+      #         provides = "custom.auto-link";
+      #       }
+      #     ];
+      #     "wireplumber.profiles" = {
+      #       main = {
+      #         "custom.auto-link" = "required";
+      #       };
+      #     };
+      #   };
+      # };
+      # extraScripts = {
+      #   "custom/auto-link.lua" = builtins.readFile ./auto-link.lua;
+      # };
     };
   };
 }
